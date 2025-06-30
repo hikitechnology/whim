b86545d0653aa8077da65afece980ebb69dfb1c8
hikipilling <hikipilled@proton.me>
2025-06-30 12:40:15 -0400

    header collapse on scroll animation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,26 @@
-import { ScrollView, StyleSheet, View } from "react-native";
+import {
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  ScrollView,
+  StyleSheet,
+  View,
+} from "react-native";
+import { useSharedValue } from "react-native-reanimated";
 
-import Overview from "@/components/Overview";
+import Overview, { BASE_OVERVIEW_HEIGHT } from "@/components/Overview";
 import UserCard from "@/components/UserCard";
 
 export default function Index() {
+  const scrollOffset = useSharedValue(0);
+
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    scrollOffset.value = event.nativeEvent.contentOffset.y;
+  };
+
   return (
     <View style={styles.container}>
-      <Overview />
-      <ScrollView>
+      <Overview scrollOffset={scrollOffset} />
+      <ScrollView style={styles.scrollContainer} onScroll={onScroll}>
         <View style={styles.profileList}>
           <UserCard
             name="Sarah Chen"
@@ -44,6 +57,9 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fffbeb",
     flex: 1,
+  },
+  scrollContainer: {
+    paddingTop: BASE_OVERVIEW_HEIGHT,
   },
   profileList: {
     padding: 16,
--- components/Overview.tsx
+++ components/Overview.tsx
@@ -3,14 +3,55 @@ import { LinearGradient } from "expo-linear-gradient";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import CountShowcase from "./CountShowcase";
+import Animated, {
+  SharedValue,
+  useAnimatedStyle,
+  interpolate,
+  Extrapolation,
+} from "react-native-reanimated";
+
+const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
 
 const overviewData = {
   peopleMet: 5,
   placesVisited: 4,
 };
 
-export default function Overview() {
+export const BASE_OVERVIEW_HEIGHT = 280;
+
+type Props = {
+  scrollOffset: SharedValue<number>;
+};
+
+export default function Overview({ scrollOffset }: Props) {
   const insets = useSafeAreaInsets();
+
+  const animatedStyles = {
+    container: useAnimatedStyle(() => ({
+      height: Math.max(BASE_OVERVIEW_HEIGHT - scrollOffset.value, insets.top),
+    })),
+    innerLayout: useAnimatedStyle(() => {
+      const radius = interpolate(
+        scrollOffset.value,
+        [0, BASE_OVERVIEW_HEIGHT - insets.top],
+        [20, 0],
+        Extrapolation.CLAMP,
+      );
+
+      return {
+        borderBottomLeftRadius: radius,
+        borderBottomRightRadius: radius,
+      };
+    }),
+    innerElements: useAnimatedStyle(() => ({
+      opacity: interpolate(
+        scrollOffset.value,
+        [0, BASE_OVERVIEW_HEIGHT - insets.top],
+        [1, 0],
+        Extrapolation.CLAMP,
+      ),
+    })),
+  };
 
   const date = new Date();
   const prettyDate = date.toLocaleDateString("en-US", {
@@ -20,44 +61,61 @@ export default function Overview() {
   });
 
   return (
-    <LinearGradient
-      colors={["rgba(255, 237, 213, 0.9)", "rgba(254, 249, 195, 0.9)"]}
-      style={[styles.container, { paddingTop: insets.top + 20 }]}
-    >
-      <Text style={styles.header}>Today&apos;s Connections</Text>
-      <Text style={styles.subheader}>{prettyDate}</Text>
-      <View style={styles.statsContainer}>
-        <CountShowcase
-          number={overviewData.peopleMet}
-          text="people met"
-          onPress={() => console.log("people met pressed")}
-        />
-        <CountShowcase
-          number={overviewData.placesVisited}
-          text="places visited"
-          onPress={() => console.log("places visited pressed")}
-        />
-      </View>
-    </LinearGradient>
+    <Animated.View style={[styles.container, animatedStyles.container]}>
+      <AnimatedLinearGradient
+        colors={["rgba(255, 237, 213, 0.9)", "rgba(254, 249, 195, 0.9)"]}
+        style={[
+          styles.innerLayout,
+          { paddingTop: insets.top },
+          animatedStyles.innerLayout,
+        ]}
+      >
+        <Animated.View
+          style={[styles.innerElements, animatedStyles.innerElements]}
+        >
+          <Text style={styles.header}>Today&apos;s Connections</Text>
+          <Text style={styles.subheader}>{prettyDate}</Text>
+          <View style={styles.statsContainer}>
+            <CountShowcase
+              number={overviewData.peopleMet}
+              text="people met"
+              onPress={() => console.log("people met pressed")}
+            />
+            <CountShowcase
+              number={overviewData.placesVisited}
+              text="places visited"
+              onPress={() => console.log("places visited pressed")}
+            />
+          </View>
+        </Animated.View>
+      </AnimatedLinearGradient>
+    </Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingBottom: 28,
+    position: "absolute",
+    top: 0,
+    right: 0,
+    left: 0,
+    height: BASE_OVERVIEW_HEIGHT,
+    zIndex: 999,
+  },
+  innerLayout: {
+    flex: 1,
+    justifyContent: "center",
     alignItems: "center",
-    gap: 16,
-    borderBottomRightRadius: 20,
     borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
     borderColor: "rgba(254, 215, 170, 0.5)",
     borderWidth: 1,
   },
-  background: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+  innerElements: {
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 16,
+    height: BASE_OVERVIEW_HEIGHT,
   },
   header: {
     fontSize: 28,
