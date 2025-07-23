e63cc983cf4b55bc7199dbc6d9ba1adae695325c
hikipilling <hikipilled@proton.me>
2025-07-23 18:56:48 -0400

    respect hidden profile section settings

--- app/(tabs)/(profile)/index.tsx
+++ app/(tabs)/(profile)/index.tsx
@@ -11,7 +11,34 @@ export default function Profile() {
 
   const loadProfile = useCallback(() => {
     if (user) {
-      getUserProfile(user.uid).then(setProfile).catch(console.error);
+      getUserProfile(user.uid)
+        .then((profile) => {
+          const hiddenKeys = Object.keys(profile)
+            // step 1: find keys starting with "show" that aren't set to true
+            .filter(
+              (key) =>
+                key.substring(0, 4) === "show" &&
+                profile[key as keyof UserProfileType] !== true,
+            )
+            // step 2: get name of property it refers to ("showFavorites" => "favorites")
+            .map((key) => key[4].toLowerCase() + key.substring(5));
+
+          // step 3: get all keys that aren't disabled
+          const shownKeys = Object.keys(profile).filter(
+            (key) => !hiddenKeys.includes(key),
+          );
+
+          // step 4: create a new object with only enabled keys and their original values
+          const profileWithoutHidden = Object.fromEntries(
+            shownKeys.map((key) => [
+              key,
+              profile[key as keyof UserProfileType],
+            ]),
+          );
+
+          setProfile(profileWithoutHidden as UserProfileType);
+        })
+        .catch(console.error);
     }
   }, [user]);
 
